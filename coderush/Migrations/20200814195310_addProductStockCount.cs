6059d877d04a0b750a7edfd029577fee8d885a6c
murat oğuz <muratoguz@gmail.com>
2020-08-14 23:15:48 +0300

    Product tablosuna StockCount eklendi

---
+++ coderush/Migrations/20200814195310_addProductStockCount.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace coderush.Migrations
+{
+    public partial class addProductStockCount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StockCount",
+                table: "Product",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StockCount",
+                table: "Product");
+        }
+    }
+}
